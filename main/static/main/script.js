d2ecc6d28774389f12faf319b105a3addb1bc79b
Home <annypodkaluk@gmail.com>
2025-05-09 23:25:52 +0300

    asdfgh

---
+++ main/static/main/script.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById('loginModal');
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}

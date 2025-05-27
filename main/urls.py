1ba0630e4870cbb2082ebcb3579ad6a472e8f541
Home <annypodkaluk@gmail.com>
2025-05-27 12:56:50 +0300

    Static

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import index, thabest, feedback_view, like_post
 
-urlpatterns = [
-    path('', index, name='index'), path('thabest/', thabest, name='thabest'),path('api/feedback/', feedback_view, name='feedback-api'), path("like/<int:post_id>/", like_post, name="like_post"),
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
+urlpatterns = [
+    path('', index, name='index'), 
+    path('thabest/', thabest, name='thabest'),
+    path('api/feedback/', feedback_view, name='feedback-api'), 
+    path("like/<int:post_id>/", like_post, name="like_post"),
+    
 ]
+
+urlpatterns += staticfiles_urlpatterns()

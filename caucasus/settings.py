3ea81e11e296b52da5f66e6ff74d105320476c97
Home <annypodkaluk@gmail.com>
2025-05-27 12:45:02 +0300

    Static

--- caucasus/settings.py
+++ caucasus/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 
 STATIC_URL = '/main/static/'
 STATICFILES_DIRS = [BASE_DIR / 'main/static']
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
